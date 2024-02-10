636fec0c490bd53ba9b88f6586a03ba0150c150a
marcelo fernandes <marcelofernandes22pm@gmail.com>
2024-02-10 10:36:38 -0300

    feat: function search by interpolation name, code added

--- clientes.py
+++ clientes.py
@@ -10,33 +10,39 @@ class Cliente:
 
     def __str__(self):
         """
-        Método __str__: Retorna uma string com os dados do cliente.
+        Método str: Retorna uma string com os dados do cliente.
         """
         return f"Nome: {self.nome}\nIdade: {self.endereco}\nID: {self.codCliente}"
 
 
-def menu(lista):
+def menu():
     """
     Função menu: Imprime o menu de opções do programa.
     não recebe parâmetros e não retorna nada.
     """
-    print('=-' *14)
+    print('=-' * 14)
     print('[1] ADICIONAR CLIENTE\n[2] BUSCAR POR NOME\n[3] BUSCAR POR CÓDIGO\n[4] SAIR')
-    
-    while(True):
-        opc = int(input('ESCOLHA UMA OPÇÃO: '))
-        print('=-' *14)
-        if opc == 1:
-            cliente = preenche(lista)
-            escreverArquivo(cliente)
-        elif opc == 2:
-            imprime(lista)
-        elif opc == 3:
-            arquivo = open("clientes.txt", "r")
-            print(arquivo.read())
-        elif opc == 4:
-            fecharArquivo(arquivo)
-            break
+
+
+def lerArquivo():
+    try:
+        with open("clientes.txt", "r") as arquivo:
+            linhas = arquivo.readlines()
+            lista_clientes = []
+            for linha in linhas:
+                nome, endereco, codCliente = linha.strip().split(',')
+                cliente = Cliente(nome, endereco, int(codCliente))
+                lista_clientes.append(cliente)
+            return lista_clientes
+    except FileNotFoundError:
+        return []
+
+
+def escreverArquivo(cliente):
+    """
+    Função escreverArquivo: Escreve os dados do cliente no arquivo clientes.txt."""
+    with open("clientes.txt", "a") as arquivo:
+        arquivo.write(f'{cliente.nome}, {cliente.endereco}, {cliente.codCliente}\n')
 
 
 def preenche(lista):
@@ -50,23 +56,49 @@ def preenche(lista):
     codCliente = int(input("ID: "))
     cliente = Cliente(nome, endereco, codCliente)
     lista.append(cliente)
+    escreverArquivo(cliente)
+    
     return cliente
 
 
-def escreverArquivo(cliente):
-    with open("clientes.txt", "a") as arquivo:
-        arquivo.write(f"{cliente}\n")
+def busca_interpolacao_nomes(lista_nomes, chave):
+    lista_clientes = lista_nomes
+    lista_nomes = [cliente.nome for cliente in lista_clientes]
+    lista_nomes.sort()
+    inicio = 0
+    fim = len(lista_nomes) - 1
+
+    while inicio <= fim:
+        # Calcula a posição utilizando uma média ponderada das posições dos caracteres
+        posicao = inicio + int(((ord(chave[0]) - ord(lista_nomes[inicio][0])) / (
+            ord(lista_nomes[fim][0]) - ord(lista_nomes[inicio][0]))) * (fim - inicio))
+
+        if lista_nomes[posicao] == chave:
+            return posicao  # Chave encontrada
+        elif lista_nomes[posicao] < chave:
+            inicio = posicao + 1
+        else:
+            fim = posicao - 1
+
+    return -1  # Chave não encontrada
 
 
-def imprime(lista):
-    """
-    Função imprime: Imprime a lista de clientes.
-    """
-    print("Lista de Clientes:")
-    for clientes in lista:
-        print(clientes)
-    return print(f'Clintes cadastrados {len(lista)}')
+def busca_interpolacao_codigos(lista_codigos, chave):
+    lista_clientes = lista_codigos
+    lista_codigos = [cliente.codCliente for cliente in lista_clientes]
+    lista_codigos.sort()
+    inicio = 0
+    fim = len(lista_codigos) - 1
 
-def fecharArquivo(arquivo):
-    arquivo.close()
-    return print('Arquivo fechado com sucesso!')
+    while inicio <= fim and lista_codigos[inicio] <= chave <= lista_codigos[fim]:
+        # Calcula a posição utilizando uma média ponderada das posições dos códigos
+        posicao = inicio + int(((chave - lista_codigos[inicio]) / (
+            lista_codigos[fim] - lista_codigos[inicio])) * (fim - inicio))
+        if lista_codigos[posicao] == chave:
+            return posicao  # Chave encontrada
+        elif lista_codigos[posicao] < chave:
+            inicio = posicao + 1
+        else:
+            fim = posicao - 1
+
+    return -1
--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-from clientes import Cliente, preenche, imprime, escreverArquivo, menu
+from clientes import Cliente, preenche, busca_interpolacao_nomes, menu, escreverArquivo, busca_interpolacao_codigos, lerArquivo
 
-lista = []
+lista = lerArquivo()
 
-menu(lista)
+while (True):
+    menu()
+    opc = int(input('ESCOLHA UMA OPÇÃO: '))
+    print('=-' * 14)
+    if opc == 1:
+        cliente = preenche(lista)
+    elif opc == 2:
+        
+        nome_busca = input('Digite o nome a ser buscado: ')
+        resultado = busca_interpolacao_nomes(lista, nome_busca)
+        if resultado != -1:
+            print(
+                f'O nome {nome_busca} foi encontrado na posição {resultado}.')
+        else:
+            print(f'O nome {nome_busca} não foi encontrado na lista.')
+    elif opc == 3:
+
+        cod_busca = int(input('Digite o código a ser buscado: '))
+        resultado = busca_interpolacao_codigos(lista, cod_busca)
+        if resultado != -1:
+            print(
+                f'O código {cod_busca} foi encontrado na posição {resultado}.')
+        else:
+            print(f'O código {cod_busca} não foi encontrado na lista.')
+    elif opc == 4:
+        break
